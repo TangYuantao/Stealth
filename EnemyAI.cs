19594932f65597d27ab77c5805c1fc6ec6c9b292
TangYuantao <1150887804@qq.com>
2017-08-03 16:54:16 +0800

    refesh

---
+++ EnemyAI.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour {
+
+	/// <summary>
+	/// 巡逻点数组
+	/// </summary>
+	public Transform[] wayPoints;
+	/// <summary>
+	/// 追捕速度
+	/// </summary>
+	public float chashingSpeed = 5f;
+	/// <summary>
+	/// 巡逻速度
+	/// </summary>
+	public float patrollingSpeed = 2f;
+	/// <summary>
+	/// 剩余距离偏移量
+	/// </summary>
+	public float remainingOffset = 0.5f;
+	/// <summary>
+	/// 机器人等待时间
+	/// </summary>
+	public float waitingTime = 3f;
+	/// <summary>
+	/// 玩家
+	/// </summary>
+	private Transform player;
+	/// <summary>
+	/// 玩家血量
+	/// </summary>
+	private PlayerHealth playerHealth;
+	/// <summary>
+	/// 玩家视觉脚本
+	/// </summary>
+	private EnemySight enemySight;
+	/// <summary>
+	/// 公共警报脚本
+	/// </summary>
+	private LastPlayerSighting lastPlayerSighting;
+	/// <summary>
+	/// 导航组件
+	/// </summary>
+	private NavMeshAgent nav;
+	/// <summary>
+	/// 计时器
+	/// </summary>
+	private float timer;
+	/// <summary>
+	/// 索引号
+	/// </summary>
+	private int index;
+
+	void Start()
+	{
+		player = GameObject.FindWithTag (Tags.Player).transform;
+		playerHealth = player.GetComponent<PlayerHealth> ();
+		enemySight = GetComponent<EnemySight> ();
+		lastPlayerSighting = GameObject.FindWithTag (Tags.GameController).
+			GetComponent<LastPlayerSighting> ();
+		nav = GetComponent<NavMeshAgent> ();
+	}
+
+	void Update()
+	{
+		//看到玩家，且玩家活着
+		if (enemySight.playerInSight && playerHealth.health > 0) {
+			//射击
+			Shooting();
+		} 
+		//是否有一个警报位置
+		else if (enemySight.personalAlarmPosition != lastPlayerSighting.normalPosition) {
+			//追捕
+			Chashing ();
+		} else {
+			//巡逻
+			Patrolling();
+		}
+	}
+	/// <summary>
+	/// 射击
+	/// </summary>
+	void Shooting()
+	{
+		//停止导航
+		nav.isStopped = true;
+	}
+	/// <summary>
+	/// 追捕
+	/// </summary>
+	void Chashing()
+	{
+		//恢复导航
+		nav.isStopped = false;
+		//设置追捕速度
+		nav.speed = chashingSpeed;
+		//设置导航目标
+		nav.SetDestination (enemySight.personalAlarmPosition);
+		//达到的目标
+		if (nav.remainingDistance < nav.stoppingDistance + remainingOffset) {
+			//开始计时
+			timer += Time.deltaTime;
+			//等待时间结束
+			if (timer >= waitingTime) {
+				//警报解除
+				lastPlayerSighting.alarmPosition = lastPlayerSighting.normalPosition;
+				//解除机器人警报
+				enemySight.personalAlarmPosition = lastPlayerSighting.normalPosition;
+				//重启计时器
+				timer = 0;
+			}
+		} else {
+			//重启计时器
+			timer = 0;
+		}
+	}
+	/// <summary>
+	/// 巡逻
+	/// </summary>
+	void Patrolling()
+	{
+		//恢复导航
+		nav.isStopped = false;
+		//设置导航速度
+		nav.speed = patrollingSpeed;
+		//设置导航目标
+		nav.SetDestination (wayPoints [index].position);
+		//如果到达了目标
+		if (nav.remainingDistance < nav.stoppingDistance + remainingOffset) {
+			//开始计时
+			timer += Time.deltaTime;
+			//计时结束
+			if (timer >= waitingTime) {
+				//切换到下一个巡逻点
+				index = ++index % wayPoints.Length;
+				//重启计时器
+				timer = 0;
+			}
+		}
+	}
+}
---
+++ EnemyAnimation.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAnimation : MonoBehaviour {
+
+	/// <summary>
+	/// 死角空间
+	/// </summary>
+	public float deadZone = 4f;
+	/// <summary>
+	/// 动画组件
+	/// </summary>
+	private Animator ani;
+	/// <summary>
+	/// 导航组件
+	/// </summary>
+	private NavMeshAgent nav;
+	/// <summary>
+	/// 玩家
+	/// </summary>
+	private Transform player;
+
+	void Start()
+	{
+		ani = GetComponent<Animator> ();
+		nav = GetComponent<NavMeshAgent> ();
+		player = GameObject.FindWithTag (Tags.Player).transform;
+	}
+
+	void Update()
+	{
+		//速度=期望速度在机器人前方的投影向量的模
+		float speed = Vector3.Project (nav.desiredVelocity, transform.forward).magnitude;
+		//角速度大小=期望速度与机器人前方的夹角
+		float angularSpeed = Vector3.Angle(nav.desiredVelocity,transform.forward);
+		//求真正的角速度（有大小有方向）
+		angularSpeed = FindAngle (angularSpeed);
+		//如果没有导航目标，让机器人Idle
+		if (nav.desiredVelocity == Vector3.zero) {
+			angularSpeed = 0;
+		}
+		//设置到动画参数
+		ani.SetFloat (HashIDs.Speed, speed);
+		ani.SetFloat (HashIDs.AngularSpeed, angularSpeed);
+	}
+
+	void OnAnimatorMove()
+	{
+		nav.velocity = ani.deltaPosition / Time.deltaTime;
+		transform.rotation = ani.rootRotation;
+	}
+
+	/// <summary>
+	/// 计算弧度.
+	/// </summary>
+	/// <returns>弧度.</returns>
+	/// <param name="angular">角度.</param>
+	float FindAngle(float angular)
+	{
+		//进入死角
+		if (angular <= deadZone) {
+			//直接看向目标
+			transform.LookAt (transform.position + nav.desiredVelocity);
+			//停止转身动画
+			return 0;
+		}
+		//求法向量
+		Vector3 normal = Vector3.Cross (transform.forward, nav.desiredVelocity);
+		//目标在左侧
+		if (normal.y < 0) {
+			//角度取反
+			angular = -angular;
+		}
+		//角度转弧度
+		angular *= Mathf.Deg2Rad;
+		//返回
+		return angular;
+	}
+}
